d491c3c0a9237692ba51a1d34d0a01334a9d3b60
Jaxon Porter <jaxonutah@gmail.com>
2019-05-10 11:39:38 -0600

    copmleted maxchar/palindrome/reverseint/reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reverse().join('')
 
-module.exports = reverse;
+}
+
+const alsoReverse = (str) => {
+  str = str.split('')
+  let newStr = []
+  for(let i=0; i<str.length; i++){
+    newStr.unshift(str[i])
+  }
+  return newStr.join('')
+}
+
+const red = (str) => {
+  return str.split('').reduce((rev, char) => char + rev, '')
+}
+
+
+
+module.exports = red;
